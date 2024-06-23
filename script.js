cb814400a64c91a2e729b08e7a23351ce614829a
Anubhav Mishra <mishra.anubhav108@gmail.com>
2024-06-23 22:38:07 +0530

    Implemented Shopping list search

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ const newItemButton = document.querySelector(".left div .add");
 newItemButton.addEventListener("click", ()=>{
     const listItem = document.createElement("li");
     listItem.textContent = newItemButton.previousElementSibling.value;
-    newItemButton.previousElementSibling.value = "";
-    const listItemContainer = document.querySelector(".left ul");
-    listItemContainer.appendChild(listItem);
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    listItem.appendChild(deleteButton);
-    deleteButton.style.marginLeft="5px";
+    if (newItemButton.previousElementSibling.value != ""){
+        newItemButton.previousElementSibling.value = "";
+        const listItemContainer = document.querySelector(".left ul");
+        listItemContainer.appendChild(listItem);
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        listItem.appendChild(deleteButton);
+        deleteButton.style.marginLeft="5px";
+    }
 })
 
 const deleteButtonContainer = document.querySelector(".left ul");
 deleteButtonContainer.addEventListener("click", (e) =>{
-    e.target.parentElement.remove();
+    if (e.target.tagName === "BUTTON"){
+        e.target.parentElement.remove();
+    }
 })
 
 const saveListButton = document.querySelector(".left div .save");
@@ -28,5 +32,19 @@ saveListButton.addEventListener("click", (e)=> {
         const temp2=temp.slice(0,temp.length-6);
         rightItems.textContent = temp2.toString();
         rightUl.appendChild(rightItems);
+    }
+})
+
+const input = document.querySelector(".right input");
+input.addEventListener("input", (e)=>{
+    const query = input.value.toLowerCase();
+    const rightItem = document.querySelectorAll(".right ul li");
+    for (let item of rightItem){
+        if (item.textContent.includes(query)){
+            item.style.display = "block";
+        }
+        else{
+            item.style.display = "none";
+        }
     }
 })
