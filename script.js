9b273d22ec7d3a0754215b3292491f38f970894f
Anubhav Mishra <mishra.anubhav108@gmail.com>
2024-06-23 20:09:37 +0530

    added shopping list search

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 
-const newItemButton = document.querySelector("div button");
+const newItemButton = document.querySelector(".left div .add");
 newItemButton.addEventListener("click", ()=>{
     const listItem = document.createElement("li");
     listItem.textContent = newItemButton.previousElementSibling.value;
-    const listItemContainer = document.querySelector("ul");
+    newItemButton.previousElementSibling.value = "";
+    const listItemContainer = document.querySelector(".left ul");
     listItemContainer.appendChild(listItem);
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     listItem.appendChild(deleteButton);
     deleteButton.style.marginLeft="5px";
 })
-const deleteButtonContainer = document.querySelector("ul");
+
+const deleteButtonContainer = document.querySelector(".left ul");
 deleteButtonContainer.addEventListener("click", (e) =>{
     e.target.parentElement.remove();
+})
+
+const saveListButton = document.querySelector(".left div .save");
+saveListButton.addEventListener("click", (e)=> {
+    const rightUl = document.querySelector(".right ul");
+    rightUl.innerHTML = "";
+    const listItem = document.querySelectorAll(".left ul li");
+    for (let item of listItem){
+        const rightItems = document.createElement("li");
+        const temp=item.textContent;
+        const temp2=temp.slice(0,temp.length-6);
+        rightItems.textContent = temp2.toString();
+        rightUl.appendChild(rightItems);
+    }
 })
